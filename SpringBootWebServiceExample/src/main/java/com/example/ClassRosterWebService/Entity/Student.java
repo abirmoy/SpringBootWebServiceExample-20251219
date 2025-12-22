0295f22c9d64c3f867b73a3fd54965ee60128831
abirmoy <n.abirmoy@gmail.com>
2025-12-22 03:57:39 +0000

    V2.0 Fully functional

--- SpringBootWebServiceExample/src/main/java/com/example/ClassRosterWebService/Entity/Student.java
+++ SpringBootWebServiceExample/src/main/java/com/example/ClassRosterWebService/Entity/Student.java
@@ -1,4 +1,78 @@
 package com.example.ClassRosterWebService.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private List<Course> courses; // Courses student is enrolled in
+
+    public Student() {
+        this.courses = new ArrayList<>();
+    }
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courses = new ArrayList<>();
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+    
+    public void addCourse(Course course) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+        courses.add(course);
+    }
+    
+    public void removeCourse(Course course) {
+        if (courses != null) {
+            courses.remove(course);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", courses=" + (courses != null ? courses.size() : 0) +
+                '}';
+    }
 }
